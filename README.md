7e6da24b177b691cf50e24ba9e4b1ebdf4493e7f
Joel McFadden <mcf.joel@gmail.com>
2015-06-23 22:21:32 -0600

    Add README, COPYING, and TODO

---
+++ README.md
@@ -0,0 +1,15 @@
+# Xoria
+Xoria is a simple sci-fi roguelike written in C++ using [The Doryen Library](http://roguecentral.org/doryen/libtcod/).
+
+![screen](/img/screen.png?raw=true "Screenshot")
+
+## Dependencies
+The Doryen Library (libtcod) must be installed in order to compile and run Xoria.
+* Download [libtcod 1.5.1](http://roguecentral.org/doryen/libtcod/download/)
+* Arch linux users can find libtcod in the [AUR](https://aur.archlinux.org/packages/libtcod/)
+
+## Building & Running
+The quickest way to build Xoria is to open **xoria.pro** in a recent version of Qt Creator (currently v3.4.1). After building, Xoria should be run from the project directory (which contains this README file) to ensure that it is able to find the necessary fonts and resources.
+
+## Notes
+Xoria is in the earliest stages of development and is **not** a functional game yet. Progress and upcoming features are summarized [here](TODO).
--- src/engine.h
+++ src/engine.h
@@ -1,28 +1,31 @@
 /*
- * Project: Xoria
- * File Name: engine.h
- * Author: Joel McFadden
- * Created: June 19, 2015
- * Last Modified: June 20, 2015
- *
- * Description: A simple sci-fi roguelike.
- *
- * Copyright (C) Joel McFadden 2015
- * License:
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+Project: Xoria
+File: engine.h
+Author: Joel McFadden
+Created: June 19, 2015
+Last Modified: June 23, 2015
+
+Description:
+    A simple sci-fi roguelike.
+
+Copyright (C) 2015 Joel McFadden
+
+Usage Agreement:
+    This file is part of Xoria.
+
+    Xoria is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Xoria is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Xoria.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef ENGINE_H
 #define ENGINE_H
